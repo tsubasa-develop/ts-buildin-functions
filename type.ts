7d91f6efad5c58b98e399b9387de83508e40dc1b
Tsubasa Ohtshka <t.ohtsuka@re-tech.net>
2022-04-01 14:50:36 +0900

    update:

--- type.ts
+++ type.ts
@@ -4,19 +4,31 @@
 
 interface Shop {
   name: string;
-  //   category: ShopCategory;
-  color?: string;
   location: string;
-  //   number: number;
-  //   barcodeSettlement: boolean;
+  color?: string;
 }
 
-interface ShopCategory {
-  id: number;
-  name: string;
-}
+/* ------------------------------------------------------------------------------------
 
-// ------------------------------------------------------------------------------------
+・Partial                 | ☆☆☆☆  | 実装者向け
+・Required                | ☆☆☆☆  | 実装者向け
+・Readonly                | ☆☆☆☆☆ | 実装者向け
+・Pick                    | ☆☆☆☆☆ | 実装者向け
+・Omit                    | ☆☆☆☆☆ | 実装者向け
+・Record                  | ☆☆☆   | 実装者向け
+・Exclude                 | ☆☆☆☆  | 実装者向け
+・Extract                 | ☆☆☆☆  | 実装者向け
+・NonNullable             | ☆☆☆☆  | 実装者向け
+・Parameters              | ☆☆☆☆  | レビュアー向け?
+・ConstructorParameters   | ☆☆☆☆  | レビュアー向け?
+・ReturnType              | ☆☆☆☆☆ | レビュアー向け?
+・InstanceType            | ☆     | レビュアー向け?
+・ThisType                | ☆☆☆☆☆ | 実装者向け
+
+  参考文献
+  https://log.pocka.io/ja/posts/typescript-builtin-type-functions/
+
+// ------------------------------------------------------------------------------------*/
 
 /*
  * Partial
